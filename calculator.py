2361f0e9a1b241fce36a0bd9f6991d7a6ab5fbc4
Chakhna <chaxnashvilig@gmail.com>
2024-11-05 19:06:58 +0400

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 def calculator(a,b):
     summary = a + b
     namravli = a*b
-    sxvaoba = a-b
-    ganayofi = a/b
-    print("jamia: ",summary, "namravlia: ",namravli, "sxvaobaa: ", sxvaoba, "ganayofia: ", ganayofi)
-calculator(10,5)
+   
+    print("jamia: ",summary, "namravlia: ",namravli)
+calculator(0,1)
